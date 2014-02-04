193f46fdf18f6030b491de6a65bbc6ee3f2c5941
nvtweak <nvtweak@linksysinfo.org>
2014-02-04 16:55:42 -0500

    set discover_retries back to 5 as in other branches

--- release/src/router/busybox/networking/udhcp/dhcpc.c
+++ release/src/router/busybox/networking/udhcp/dhcpc.c
@@ -1269,7 +1269,7 @@ int udhcpc_main(int argc UNUSED_PARAM, char **argv)
 	llist_t *list_x = NULL;
 	int tryagain_timeout = 20;
 	int discover_timeout = 3;
-	int discover_retries = 3;
+	int discover_retries = 5;
 	uint32_t server_addr = server_addr; /* for compiler */
 	uint32_t requested_ip = 0;
 	uint32_t xid = xid; /* for compiler */
